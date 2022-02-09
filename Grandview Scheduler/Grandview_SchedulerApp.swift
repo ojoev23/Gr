00d73f1b38cbc12bda3ff0433886cb00a1294bb8
Joe Olapurath <ojoev23@gmail.com>
2022-02-08 18:32:07 -0700

    Initial Commit

---
+++ Grandview Scheduler/Grandview_SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grandview_SchedulerApp.swift
+//  Grandview Scheduler
+//
+//  Created by Joe Olapurath on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct Grandview_SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
